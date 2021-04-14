0a83a2256c150a3fa23e8e07f0f373e5bf1db14e
shindonghyun <donghyun5349@naver.com>
2021-04-15 02:55:41 +0900

    typescript_3

---
+++ typescript_3/test.ts
@@ -0,0 +1,33 @@
+// let a = "Mark";
+
+// a = 39; // 위에서 String으로 정의되었기 때문에 숫자로 바뀌지 못함 String만 가능
+
+
+// let a; // any 타입이 됨
+
+// a = 39; // 가능
+
+
+// let a: string; // 이렇게 타입을 달아주는것이 Annotation
+
+// a = "Mark" // 가능
+
+// a = 39; // 불가능
+
+
+// let a: number; 
+
+// a = "Mark" // 불가능
+
+// a = 39; // 가능
+
+
+// // b는 숫자만 가능
+// function hello(b: number) {
+
+// }
+
+// hello(39) // 가능
+// hello('39') // 불가능
+
+
